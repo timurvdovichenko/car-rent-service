a63285955eacc31a100f721b156cab570f2874cc
timurvdovichenko <32131557+timurvdovichenko@users.noreply.github.com>
2023-10-10 15:38:58 +0300

    Initial commit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,52 @@
+import { deleteContact, fetchContacts, addContact } from './operations';
+
+const { createSlice } = require('@reduxjs/toolkit');
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const contactsInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  extraReducers: {
+    [fetchContacts.pending]: handlePending,
+    [fetchContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+    },
+    [fetchContacts.rejected]: handleRejected,
+
+    [deleteContact.pending]: handlePending,
+    [deleteContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const index = state.items.findIndex(item => item.id === action.payload.id);
+      state.items.splice(index, 1);
+    },
+    [deleteContact.rejected]: handleRejected,
+    [addContact.pending]: handlePending,
+    [addContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items.push(action.payload);
+    },
+    [addContact.rejected]: handleRejected,
+  },
+});
+
+export const contactsReducer = contactsSlice.reducer;
+
+export const getContacts = state => state.contacts.items;
